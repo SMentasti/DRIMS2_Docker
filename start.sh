a15d5af5a59ccb0af88a7d9b1bdff8767299a2ff
Cesare Tonola <c.tonola001@unibs.it>
2025-08-12 16:54:59 +0200

    wip

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 
 # Specify the container name
 CONTAINER_NAME="drims2"
-IMAGE_NAME="smentasti/drims2:2025"
+IMAGE_NAME="cf5f52ef57e3"
 #IMAGE_NAME="my_image"
 
 # Pull the latest image
 echo "Pulling the latest image: $IMAGE_NAME..."
-docker pull $IMAGE_NAME
+#docker pull $IMAGE_NAME
 
 # Grant X permissions
 xhost +si:localuser:$(whoami)
@@ -29,5 +29,22 @@ else
     echo "Container $CONTAINER_NAME does not exist."
 fi
 
-docker run -it  --user drims --privileged -v /dev:/dev -v /dev/bus/usb:/dev/bus/usb --device=/dev/bus/usb --device-cgroup-rule='c 189:* rmw'  -v /etc/udev/rules.d:/etc/udev/rules.d --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --net=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/drims_ws:/home/drims/drims_ws" --volume="$(pwd)/bags:/home/drims/bags"  --name drims2 -w /home/drims $IMAGE_NAME
+#docker run -it  --user drims --privileged -v /dev:/dev -v /dev/bus/usb:/dev/bus/usb --device=/dev/bus/usb --device-cgroup-rule='c 189:* rmw'  -v /etc/udev/rules.d:/etc/udev/rules.d --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --net=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/drims_ws:/home/drims/drims_ws" --volume="$(pwd)/bags:/home/drims/bags"  --name drims2 -w /home/drims $IMAGE_NAME
+
+
+docker run -it --privileged --net=host \
+  --user drims \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=/home/drims/.Xauthority \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v "$XAUTHORITY":/home/drims/.Xauthority:ro \
+  -v /dev:/dev \
+  -v /dev/bus/usb:/dev/bus/usb --device=/dev/bus/usb --device-cgroup-rule='c 189:* rmw' \
+  -v /etc/udev/rules.d:/etc/udev/rules.d \
+  -v "$(pwd)/drims_ws:/home/drims/drims_ws" \
+  -v "$(pwd)/bags:/home/drims/bags" \
+  --name drims2 \
+  -w /home/drims \
+  "$IMAGE_NAME"
 
